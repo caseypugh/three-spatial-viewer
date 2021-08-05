45270e62701bd00e3163dbf422f0cf926cf54258
caseypugh <me@caseypugh.com>
2021-08-05 13:54:24 -0400

    dist updates

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 import Player from "./components/player";
-import { QuiltConfig, SpatialType, StereoMode } from "./components/constants";
-export { Player, QuiltConfig, SpatialType, StereoMode };
+import { QuiltConfig, SpatialType, StereoMode, Props } from "./components/constants";
+export { Player, QuiltConfig, SpatialType, StereoMode, Props };
